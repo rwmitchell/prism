76b6a6678f1c366c68d14542c2b0ea54adf8e82e
Richard Mitchell <rwmitchell@gmail.com>
2019-02-15 12:05:36 -0500

    use dirname_r instead to avoid YCM errors

--- Source/bugout.h
+++ Source/bugout.h
@@ -1,6 +1,8 @@
 #include <libgen.h>
+#include <dirent.h>
+char buf[MAXNAMLEN];
 #define BUGOUT( FMT, ... ) { \
-  fprintf(stdout, "%s: %5d:%-24s:", basename(__FILE__), __LINE__, __func__ ); \
+  fprintf(stdout, "%s: %5d:%-24s:", basename_r(__FILE__, buf), __LINE__, __func__ ); \
   fprintf(stdout, FMT, ##__VA_ARGS__ ); \
   fflush (stdout); \
 }
